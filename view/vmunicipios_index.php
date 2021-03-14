5baf12eb069528be6b06e7444006fa49c9a69ef5
DanielS <daessar12@gmail.com>
2021-03-14 13:45:26 -0500

    Todo el crud de municipios funcionando con algunas modificaciones esteticas en el diseño

--- view/vmunicipios_index.php
+++ view/vmunicipios_index.php
@@ -6,8 +6,9 @@
     </div>
   <?php } ?>
   <h2>Listado Municipios</h2>
+  <div class="d-flex">
     <table class="table">
-      <thead>
+      <thead class="bg-primary">
         <tr>
           <th scope="col">Id</th>
           <th scope="col">Nombre</th>
@@ -16,16 +17,17 @@
       </thead>
       <tbody>
       <?php foreach ($municipios as $municipio) {  ?> 
-        <tr>
+        <tr class="text-right">
           <td><?= $municipio -> id?></td>
           <td><?= $municipio -> nombre?></td>
           <td>
-            <a href="municipios_editar.php?id=<?= $municipio -> id?>" class="btn btn-primary active" aria-current="page">Actualizar</a>
+            <a href="municipios_editar.php?id=<?= $municipio -> id?>" class="btn btn-success active" aria-current="page">Actualizar</a>
             <a href="municipios_destroy.php?id=<?= $municipio -> id?>" class="btn btn-danger active" aria-current="page">Eliminar</a>
           </td>
         </tr>
       <?php } ?>
       </tbody>
     </table>
+  </div>
   </div>
 </section>
